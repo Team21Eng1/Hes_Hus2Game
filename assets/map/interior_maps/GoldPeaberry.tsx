4bb2aaafdf8495d9dd9936c9dc7bc8954ce4e465
Surbhi <lahoriasurbhi@gmail.com>
2024-04-29 20:42:36 +0100

    added interior maps

---
+++ assets/map/interior_maps/GoldPeaberry.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="GoldPeaberry" tilewidth="16" tileheight="16" tilecount="368" columns="16">
+ <image source="interior_assests/GoldPeaberry.png" width="268" height="368"/>
+</tileset>
---
+++ assets/map/interior_maps/RedPeaberry-preview.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="RedPeaberry-preview" tilewidth="16" tileheight="16" tilecount="182" columns="14">
+ <image source="interior_assests/RedPeaberry-preview.png" width="224" height="216"/>
+</tileset>
