0c9f6d7f945dbea18378a8274e70193b54eccb77
Surbhi <lahoriasurbhi@gmail.com>
2024-04-29 20:56:46 +0100

    updated map name

--- assets/map/interior_maps/Food.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="FOOD" tilewidth="16" tileheight="16" tilecount="2150" columns="43">
- <image source="interior_assests/FOOD.png" width="702" height="807"/>
-</tileset>
